cb61d95713ed69c3496dfa3aebfc919b0ad99266
accamper <accamper@users.noreply.github.com>
2015-10-06 00:32:29 -0700

    Added shortcut handling

--- background.js
+++ background.js
@@ -1,9 +1,32 @@
 window.console.log('What is noise? Is it background music?');
 // TODO On install make sure to refresh all spotify pages. 
-// TODO This page will only be needed for the chrome.commands.
-// TODO Make sure to set the media keys as default. This should be fun.
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ? "content" : "extension?");
-      console.log(request.ping);
-    });
+// This page is only needed for the chrome.commands.
+chrome.commands.onCommand.addListener(function(command) {
+  var msg = '';
+  switch (command) {
+    case 'play-pause':
+      msg = QuickifyMessages.PLAY_OR_PAUSE;
+      break;
+    case 'next':
+      msg = QuickifyMessages.NEXT;
+      break;
+    case 'previous':
+      msg = QuickifyMessages.PREVIOUS;
+      break;
+    case 'track-add':
+      msg = QuickifyMessages.SAVE;
+      break;
+    case 'repeat':
+      msg = QuickifyMessages.REPEAT;
+      break;
+    case 'shuffle':
+      msg = QuickifyMessages.SHUFFLE;
+      break;
+    default:
+      window.console.log('CANNOT HANDLE THIS COMMAND: ' + command);
+      return;
+  }
+  
+  QuickifySendToContent(msg);
+});
+
